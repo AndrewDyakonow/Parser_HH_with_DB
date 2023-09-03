cd8992b9fa2fe04a8fd7e17faf93a9c868fed28e
Blendi <dronramone@mail.ru>
2023-09-04 01:34:20 +0400

    Django first setting. Add staticfiles

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Vacancies(models.Model):
+    id_vacancy = models.PositiveIntegerField(verbose_name='ID вакансии')
+    name_company = models.ForeignKey('Company', on_delete=models.DO_NOTHING, verbose_name='Название компании')
+    name_vacancy = models.CharField(max_length=127, verbose_name='Название вакансии')
+    adress = models.CharField(max_length=255, verbose_name='Адрес')
+    url = models.URLField(verbose_name='Ссылка на вакансию')
+    salary = models.PositiveIntegerField(verbose_name='Зарплата')
+    requirement = models.TextField(verbose_name='Требования')
+    responsibiliti = models.TextField(max_length=1023, verbose_name='Ответственность')
+    date_created = models.DateTimeField(verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['id_vacancy']
+
+    def __str__(self):
+        return f'{self.name_vacancy}, {self.salary}'
+
+
+class Company(models.Model):
+    id_company = models.PositiveIntegerField(verbose_name='ID вакансии')
+    name_company = models.CharField(max_length=127, verbose_name='Название компании')
+    url = models.URLField(verbose_name='Ссылка на компанию')
+    adress = models.CharField(max_length=255, verbose_name='Адрес')
+
+    class Meta:
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+        ordering = ['name_company']
+
+    def __str__(self):
+        return f'{self.name_company}'
---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from main.apps import MainConfig
+
+app_name = MainConfig.name
+
+urlpatterns = [
+
+]
